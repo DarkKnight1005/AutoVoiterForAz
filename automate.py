9e56bc4bbe69a5512e3dcaae3862666bc425350b
DarkKnight1005 <bloodykillerplay@gmail.com>
2020-10-05 20:48:29 +0400

    first commit

---
+++ automate.py
@@ -0,0 +1,22 @@
+from selenium import webdriver
+import time
+# Using Chrome to access web
+url = input()
+
+while True:
+    driver = webdriver.Chrome(executable_path=url)
+    # Open the website
+    driver.get('https://echo.msk.ru/polls/2720047-echo.html')
+
+    id_box_select = driver.find_element_by_id('poll_a_200209')
+
+    id_box_submit = driver.find_elements_by_name("submit")
+
+    id_box_select.click()
+
+    id_box_submit[3].click()
+
+    time.sleep(10)
+
+    driver.close()
+
